e2c698b4b1721b2122c3b83d2e6defcd309b83e6
johncross00 <johncross.ktm@gmail.com>
2024-01-24 12:40:54 +0000

    Scaffold messenger snackbar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,56 +1,92 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text(
-            'Flutter Catalog',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-            ),
+        title: const Text(
+          'Flutter Catalog',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
           ),
-          elevation: 5,
-          actions: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () => {},
-                  icon: Icon(Icons.menu, color: Colors.white, size: 30,),
-                ),
-                IconButton(
-                  onPressed: () => {},
-                  icon: Icon(Icons.search, color: Colors.white, size: 30,),
-                ),
-              ],
-            ),
-          ],
-          backgroundColor: Colors.blue[900]
+        ),
+        elevation: 5,
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.menu, color: Colors.white, size: 30,),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.search, color: Colors.white, size: 30,),
+              ),
+            ],
+          ),
+        ],
+        backgroundColor: Colors.blue[900],
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 15.0),
-        child: TextField(
-          obscureText: (true),
-          // maxLines: 5,
-          keyboardType: TextInputType.number,
-          showCursor: true,
-          decoration: InputDecoration(
-            prefixIcon: Icon(Icons.search),
-            hintText: 'Enter your number',
-            icon: Icon(Icons.phone_android),
-            iconColor: Colors.red,
-            suffixIcon: Icon(Icons.tab),
-            hintStyle: TextStyle(color: Colors.black),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.red),
-              borderRadius: BorderRadius.circular(10)
+        child: Column(
+          children: [
+            TextField(
+              // obscureText: (true),
+              // maxLines: 5,
+              keyboardType: TextInputType.number,
+              showCursor: true,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Enter your number',
+                icon: Icon(Icons.phone_android),
+                iconColor: Colors.red,
+                suffixIcon: Icon(Icons.numbers),
+                hintStyle: TextStyle(color: Colors.black),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
             ),
-          )
+            // ListView(
+            //   children: [
+            //     ListTile(
+            //       leading: Text("This is the leading"),
+            //       title: Text('List'),
+            //       subtitle: Text('List'),
+            //     ),
+            //     ListTile(
+            //       title: Text('List'),
+            //       subtitle: Text('List'),
+            //     ),
+            //     ListTile(
+            //       title: Text('List'),
+            //       subtitle: Text('List'),
+            //     ),
+            //   ],
+            // ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('List'),
+              subtitle: Text('List'),
+              onLongPress: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Long press me!'),
+                  ),
+                );
+              },
+            ),
+
+          ],
         ),
       ),
     );
   }
 }
+
